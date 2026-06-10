e918cabc95fa17aaa123ad82cae0d95e60bf30d5
Sebas <sjimenezpo@unal.edu.co>
2026-06-10 18:24:24 -0500

    Demas

--- Clientes.cpp
+++ Clientes.cpp
@@ -5,6 +5,8 @@
 #include <iostream>
 #include <sstream>
 #include <fstream>
+#include <ctime> 
+#include <cstdlib>
 
 
 using namespace std;
@@ -27,14 +29,186 @@ int buscarIndiceClientePorId(int idBuscado) {
 }
 
 int generadIdClienteAleatorio(){
+
+    if(idsClientes.size()<999){
+        bool cic = true;
+    bool check = true;
+    srand(time(NULL));
+    while (cic == true){
+        check = true;
+        int num = (rand()%999) + 1;
     
-}
+        for(int comp : idsClientes){
+            if(comp == num){
+                check = false;
+            }
+        }if (check == true and num >0 ){
 
-bool registrarCliente(int id, string nombre, string telefono){
+            return num;
+        }
+    }
 
-    if(id >0){
 
     }else{
-        return false;
+        return -1;
     }
 }
+
+int registrarCliente(string nombre, string telefono) {
+
+    if (nombre == "") {
+        return -1;
+    }
+
+    if (telefono.size()!= 10) {
+        return -1;
+    }
+
+    int id = generadIdClienteAleatorio();
+
+    if (id == -1) {
+        return -1;
+    }
+
+    idsClientes.push_back(id);
+    nombresClientes.push_back(nombre);
+    telefonosClientes.push_back(telefono);
+    activosClientes.push_back(true);
+
+    return id;
+}
+
+void mostrarClientes() {
+
+    if (idsClientes.empty() or nombresClientes.empty() or telefonosClientes.empty() or activosClientes.empty()){
+        cout << "No hay clientes registrados." << endl;
+        return;
+    }
+
+    cout << "===== CLIENTES REGISTRADOS =====" << endl;
+
+    for (int i = 0; i < idsClientes.size(); i++) {
+
+        cout << "ID: " << idsClientes[i]<< endl;
+        cout << "Nombre: " <<nombresClientes[i]<< endl;
+        cout << "Telefono: " <<telefonosClientes[i]<< endl;
+
+        if (activosClientes[i] == true) {
+            cout << "Estado: ACTIVO" << endl;
+        } else {
+            cout << "Estado: INACTIVO" << endl;
+        }
+
+        cout << "----------------------------" << endl;
+    }
+}
+void mostrarClientePorId(int idBuscado) {
+
+    int posicion = buscarIndiceClientePorId(idBuscado);
+
+    if (posicion == -1) {
+        cout << "Cliente no encontrado." << endl;
+        return;
+    }
+
+    cout << "===== CLIENTE ENCONTRADO =====" << endl;
+    cout << "ID: " <<idsClientes[posicion]<< endl;
+    cout << "Nombre: " <<nombresClientes[posicion]<< endl;
+    cout << "Telefono: " <<telefonosClientes[posicion]<< endl;
+
+    if (activosClientes[posicion]) {
+        cout << "Estado: ACTIVO" << endl;
+    } else {
+        cout << "Estado: INACTIVO" << endl;
+    }
+}
+
+bool modificarCliente(int idBuscado, string nuevoNombre, string nuevoTelefono) {
+
+    int posicion = buscarIndiceClientePorId(idBuscado);
+
+
+
+    if (posicion == -1) {
+        return false;
+    }
+
+    if (nuevoNombre == "") {
+        return false;
+    }
+
+    if (nuevoNombre.size()!= 10) {
+        return false;
+    }
+
+    nombresClientes[posicion] = nuevoNombre;
+    telefonosClientes[posicion] = nuevoTelefono;
+
+    return true;
+}
+
+bool desactivarCliente(int idBuscado) {
+
+    int posicion = buscarIndiceClientePorId(idBuscado);
+
+    if (posicion == -1) {
+        return false;
+    }
+
+    if (activosClientes[posicion]== false) {
+        return false;
+    }
+
+    activosClientes[posicion] = false;
+
+    return true;
+}
+bool activarCliente(int idBuscado) {
+
+    int posicion = buscarIndiceClientePorId(idBuscado);
+
+    if (posicion == -1) {
+        return false;
+    }
+
+    if (activosClientes[posicion]== true) {
+        return false;
+    }
+
+    activosClientes[posicion] = true;
+
+    return true;
+}
+
+bool guardarClientes(string nombreArchivo) {
+
+    ofstream archivo(nombreArchivo);
+
+    if (archivo.is_open()== false) {
+        return false;
+    }
+
+    archivo << "idCliente,nombre,telefono,activo" << endl;
+
+    for (int i = 0; i < idsClientes.size(); i++) {
+
+        archivo << idsClientes[i]
+                << ","
+                << nombresClientes[i]
+                << ","
+                << telefonosClientes[i]
+                << ","
+                << activosClientes[i]
+                << endl;
+    }
+
+    archivo.close();
+
+    return true;
+}
+
+
+
+ 
+
+
--- FoodSystem/clientes.h
+++ FoodSystem/clientes.h
@@ -2,5 +2,13 @@
 
 #include <string>
 
-int buscarIndiceClientePorId(int idBusacdo);
-bool registrarCliente(int id, std::string nombre, std::string telefono);
+
+int buscarIndiceClientePorId(int idBuscado);
+int generarIdClienteAleatorio();
+int registrarCliente(std::string nombre, std::string telefono);
+void mostrarClientes();
+void mostrarClientePorId(int idBuscado);
+bool modificarCliente(int idBuscado, std::string nuevoNombre, std::string nuevoTelefono);
+bool desactivarCliente(int idBuscado);
+bool activarCliente(int idBuscado);
+bool guardarClientes(std ::string nombreArchivo);
